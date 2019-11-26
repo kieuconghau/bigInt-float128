6722596dd621126a4ef3ccafaaef780700fbcb27
hoangnhan1105 <nguyenhoangnhan0@gmail.com>
2019-11-26 23:20:25 +0700

    Add function.
    Finish TestMode function (the main function for Test mode).

--- Data_x128/TestMode.cpp
+++ Data_x128/TestMode.cpp
@@ -1,1 +1,51 @@
 #include "TestMode.h"
+
+void TestMode(char *inputFilename, char* outputFilename, int type) {
+	ifstream fin;
+	fin.open(inputFilename);
+	if (!fin.is_open()) {
+		return;
+	}
+
+	ofstream fout;
+	fout.open(inputFilename);
+	if (!fout.is_open()) {
+		return;
+	}
+
+	string data_line;
+	while (getline(fin, data_line)) {
+		vector<string> data_list;
+		int i = 0;
+
+		while (i < data_line.size()) {
+			while (data_line[i] == ' ' && i < data_line.size()) {
+				i++;
+			}
+
+			if (i == data_line.size()) break;
+
+			string data;
+			while (data_line[i] != ' ' && i < data_line.size()) {
+				data += data_line[i];
+			}
+			data_list.push_back(data);
+		}
+
+		if (data_list[0] != "2" && data_list[0] != "10" && data_list[0] != "16") {
+			break;
+		}
+		string result;
+		if (data_list[1] == "2" || data_list[1] == "10" || data_list[1] == "16") {
+			result = Conversion(stoi(data_list[0]), stoi(data_list[1]), data_list[2]);
+		}
+		else {
+			result = BinaryOperation(stoi(data_list[0]), data_list[2], data_list[1], data_list[3]);
+		}
+
+		fout << result << endl;
+	}
+
+	fout.close();
+	fin.close();
+}
--- Data_x128/TestMode.h
+++ Data_x128/TestMode.h
@@ -1,1 +1,11 @@
 #pragma once
+#include <fstream>
+#include <string>
+#include <vector>
+using namespace std;
+
+string Conversion(int p1, int p2, string operand);
+
+string BinaryOperation(int p, string operation, string operand_1, string operand_2);
+
+void TestMode(char *inputFilename, char* outputFilename, int type);
