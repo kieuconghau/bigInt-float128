d9d6b2914dc8ec7c6c93570769226594c95ab887
hoangnhan1105 <nguyenhoangnhan0@gmail.com>
2019-11-27 22:04:06 +0700

    Add functions.
    Add supporting functions for QIntConversion and QIntOperation functions.

--- Data_x128/TestMode.cpp
+++ Data_x128/TestMode.cpp
@@ -1,6 +1,111 @@
 #include "TestMode.h"
 #include "QInt.h"
 
+/* Almost the same as scanQInt */
+QInt DecStrToQInt(string dec_str) {
+	QInt qint;
+	
+	bool* bit = new bool[BIT_COUNT]();
+	decStrToBinStr(dec_str, bit, BIT_COUNT);
+	qint = binToDec(bit);
+	delete[] bit;
+
+	return qint;
+}
+
+/* Almost the same as printQInt */
+string QIntToDecStr(QInt x) {
+	string str_data[DATA_COUNT];		// 4 int's in QInt struct expressed in string form.
+	string output;			// Output string
+
+	if (isNegative(x)) {
+		output += "-";
+
+		// Convert QInt from the negative form to the positve form
+		QInt one;
+		one.data[DATA_COUNT - 1] |= 1;
+
+		if (!isMinQInt(x))		// If x is MIN_QINT, we don't need to convert it to th positive form.
+			x = ~(x - one);
+	}
+
+	// Convert 'data' in QInt to string form
+	for (int i = 0; i < DATA_COUNT; i++) {
+		str_data[i] = to_string(x.data[i]);
+	}
+
+	// Multiply str_data[2] by 2^32, str_data[1] by 2^64,
+	// and str_data[0] by 2^96
+	for (int i = 1; i < DATA_COUNT; i++) {
+		if (x.data[DATA_COUNT - 1 - i] > 0) {
+			for (int j = 0; j < 32 * i; j++) {
+				str_data[DATA_COUNT - 1 - i] = posDecStrMultiply2(str_data[DATA_COUNT - 1 - i]);
+			}
+		}
+	}
+
+	for (int i = 1; i < DATA_COUNT; i++) {
+		str_data[0] = posDecStrAddDecStr(str_data[0], str_data[i]);
+	}
+
+	output += str_data[0];
+
+	return output;
+}
+
+bool* BinStrToBoolArr(string bin_str) {
+	bool* bit = new bool[BIT_COUNT]();
+	
+	int j = BIT_COUNT - 1;
+	for (int i = bin_str.size() - 1; i >= 0; i--) {
+		bit[j] = bin_str[i];
+		j--;
+	}
+
+	return bit;
+}
+
+string BoolArrToBinStr(bool* bit) {
+	string bin_str;
+
+	int i = 0;
+	while (bit[i] == false) {
+		i++;
+	}
+
+	while (i < BIT_COUNT) {
+		bin_str += bit[i];
+		i++;
+	}
+
+	return bin_str;
+}
+
+QInt HexToDec(string hex) {
+	QInt res;
+
+	int i = hex.size() - 1;
+	int j = BIT_COUNT - 1;
+	uint32_t temp;
+
+	while (i >= 0 && j >= 0) {
+		temp = hex[i] >= '0' && hex[i] <= '9' ? hex[i] - '0' : hex[i] - 'A' + 10;
+		res.data[j / 32] |= temp << (31 - j % 32);		// 0, 4, 8, 12, 16, 20, 24, 28
+
+		--i;
+		j -= 4;
+	}
+
+	return res;
+}
+
+bool* HexToBin(string hex) {
+	QInt x = HexToDec(hex);
+	
+	return decToBin(x);
+}
+
+
 string QIntConversion(int p1, int p2, string operand) {
 	string result;
 
@@ -10,11 +115,11 @@ string QIntConversion(int p1, int p2, string operand) {
 		}
 		else {
 			bool* bit = new bool[BIT_COUNT]();
-			// bit = BinStrToBoolArr(operand)	// Binary string -> Bool*
+			bit = BinStrToBoolArr(operand);		// Binary string -> Bool*
 
 			if (p2 == 10) {
 				QInt qint = binToDec(bit);		// Bool* -> QInt
-				// result = QIntToDecStr(qint)	// QInt -> Decimal string
+				result = QIntToDecStr(qint);	// QInt -> Decimal string
 			}
 			else if (p2 == 16) {
 				result = binToHex(bit);			// Bool* -> Hexadecimal string
@@ -30,13 +135,12 @@ string QIntConversion(int p1, int p2, string operand) {
 		else if (p2 == 2) {
 			bool* bit = new bool[BIT_COUNT]();
 			decStrToBinStr(operand, bit, BIT_COUNT);	// Decimal string -> Bool*
-			// result = BoolArrToBinStr(bit)			// Bool* -> Binary string
+			result = BoolArrToBinStr(bit);				// Bool* -> Binary string
 
 			delete[] bit;
 		}
 		else if (p2 == 16) {
-			QInt qint;
-			// qint = DecStrToQInt(operand)		// Decimal string -> QInt
+			QInt qint = DecStrToQInt(operand);	// Decimal string -> QInt
 			result = decToHex(qint);			// QInt -> Hexadecimal string
 		}
 	}
@@ -46,15 +150,28 @@ string QIntConversion(int p1, int p2, string operand) {
 		}
 		else if (p2 == 2) {
 			bool* bit = new bool[BIT_COUNT]();
-			// bit = HexToBin(operand)			// Hexadecimal string -> Bool*
-			// result = BoolArrToBinStr(bit)	// Bool* -> Binary string
+			bit = HexToBin(operand);		// Hexadecimal string -> Bool*
+			result = BoolArrToBinStr(bit);	// Bool* -> Binary string
 
 			delete[] bit;
 		}
 		else if (p2 == 10) {
-			// QInt qint = HexToDec(operand)	// Hexadecimal string -> QInt
-			// result = QIntToDecStr(qint)		// QInt -> Decimal string
+			QInt qint = HexToDec(operand);	// Hexadecimal string -> QInt
+			result = QIntToDecStr(qint);	// QInt -> Decimal string
 		}
+	}
+
+	return result;
+}
+
+string QFloatConversion(int p1, int p2, string operand) {
+	string result;
+
+	if (p1 == 2 && p2 == 10) {
+
+	}
+	else if (p1 == 10 && p2 == 2) {
+
 	}
 
 	return result;
@@ -67,15 +184,15 @@ string QIntOperation(int p, string operation, string operand_1, string operand_2
 
 	if (p == 2) {
 		bool* bit = new bool[BIT_COUNT]();
-		// bit = BinStrToBoolArr(operand_1)
+		bit = BinStrToBoolArr(operand_1);
 		op_1 = binToDec(bit);
 		delete[] bit;
 	}
 	else if (p == 10) {
-		// op_1 = DecStrToQInt(operand_1)
+		op_1 = DecStrToQInt(operand_1);
 	}
 	else if (p == 16) {
-		// op_1 = HexToDec(operand_1)
+		op_1 = HexToDec(operand_1);
 	}
 
 
@@ -102,15 +219,15 @@ string QIntOperation(int p, string operation, string operand_1, string operand_2
 
 		if (p == 2) {
 			bool* bit = new bool[BIT_COUNT]();
-			// bit = BinStrToBoolArr(operand_2)
+			bit = BinStrToBoolArr(operand_2);
 			op_2 = binToDec(bit);
 			delete[] bit;
 		}
 		else if (p == 10) {
-			// op_2 = DecStrToQInt(operand_2)
+			op_2 = DecStrToQInt(operand_2);
 		}
 		else if (p == 16) {
-			// op_2 = HexToDec(operand_2)
+			op_2 = HexToDec(operand_2);
 		}
 		
 
@@ -138,7 +255,7 @@ string QIntOperation(int p, string operation, string operand_1, string operand_2
 	}
 
 	string result_string;
-	// result_string = QIntToDecStr(result_qint)
+	result_string = QIntToDecStr(result_qint);
 	return result_string;
 }
 
--- Data_x128/TestMode.h
+++ Data_x128/TestMode.h
@@ -4,6 +4,19 @@
 #include <vector>
 using namespace std;
 
+QInt DecStrToQInt(string dec_str);
+
+string QIntToDecStr(QInt x);
+
+bool* BinStrToBoolArr(string bin_str);
+
+string BoolArrToBinStr(bool* bit);
+
+QInt HexToDec(string hex);
+
+bool* HexToBin(string hex);
+
+
 string QIntConversion(int p1, int p2, string operand);
 
 string QFloatConversion(int p1, int p2, string operand);
